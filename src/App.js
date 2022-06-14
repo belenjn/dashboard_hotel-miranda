ef49d50f59833dbca3d25fd87ceb05aeba6f458b
Belen <belenjarabanavas31@gmail.com>
2022-06-14 23:36:20 +0200

    changeUsername and changeEmail finished

--- src/App.js
+++ src/App.js
@@ -13,16 +13,15 @@ import { UserDetails } from "./components/users/UserDetails";
 import { Contacts } from "./components/contacts/Contacts";
 import { ContactDetails } from "./components/contacts/ContactDetails";
 
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { createContext, useEffect, useReducer } from "react";
 
 import { RequireAuth } from "./utils/RequireAuth";
 import { Login } from "./components/login/Login";
 
-const initialUser = localStorage.getItem('authenticated') 
-  ? JSON.parse(localStorage.getItem('authenticated')) 
-  : {authenticated: false, username: null, email:null} ;
-
+const initialUser = localStorage.getItem("authenticated")
+  ? JSON.parse(localStorage.getItem("authenticated"))
+  : { authenticated: false, username: null, email: null };
 
 export const authReducer = (state, action) => {
   switch (action.type) {
@@ -37,12 +36,14 @@ export const authReducer = (state, action) => {
     case "logout":
       localStorage.removeItem("authenticated");
       return { ...state, authenticated: false };
+    case "changeUsername":
+      return { ...state, username: action.username };
+    case "changeEmail":
+      return { ...state, email: action.email };
     default:
       return state;
-    //falta cambio de email y de username
   }
 };
-
 
 export const authContext = createContext();
 
@@ -54,98 +55,94 @@ function App() {
   );
 
   useEffect(() => {
-    if(authenticated.authenticated){
-      localStorage.setItem('authenticated', JSON.stringify(authenticated));
-      navigate('/', { replace: true });  
-    }else{
-      navigate('/login', { replace: true });
+    if (authenticated.authenticated) {
+      localStorage.setItem("authenticated", JSON.stringify(authenticated));
+      navigate("/", { replace: true });
+    } else {
+      navigate("/login", { replace: true });
     }
   }, [authenticated, navigate]);
 
-
   return (
-    
-   <authContext.Provider value={{ authenticated, dispatchAuthenticated }}>
-        <Navbar />
-        <Routes>
-          <Route path="/login" exact element={<Login />} />
-          <Route
-            path="/"
-            exact
-            element={
-              <RequireAuth>
-                <Home />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/bookings"
-            element={
-              <RequireAuth>
-                <Bookings />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/bookings/id"
-            element={
-              <RequireAuth>
-                <BookingDetails />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/rooms"
-            element={
-              <RequireAuth>
-                <Rooms />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/rooms/id"
-            element={
-              <RequireAuth>
-                <RoomDetails />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/users"
-            element={
-              <RequireAuth>
-                <Users />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/users/id"
-            element={
-              <RequireAuth>
-                <UserDetails />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <RequireAuth>
-                <Contacts />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/contacts/id"
-            element={
-              <RequireAuth>
-                <ContactDetails />
-              </RequireAuth>
-            }
-          />
-        </Routes>
+    <authContext.Provider value={{ authenticated, dispatchAuthenticated }}>
+      <Navbar />
+      <Routes>
+        <Route path="/login" exact element={<Login />} />
+        <Route
+          path="/"
+          exact
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/bookings"
+          element={
+            <RequireAuth>
+              <Bookings />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/bookings/id"
+          element={
+            <RequireAuth>
+              <BookingDetails />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/rooms"
+          element={
+            <RequireAuth>
+              <Rooms />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/rooms/id"
+          element={
+            <RequireAuth>
+              <RoomDetails />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <RequireAuth>
+              <Users />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/users/id"
+          element={
+            <RequireAuth>
+              <UserDetails />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <RequireAuth>
+              <Contacts />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/contacts/id"
+          element={
+            <RequireAuth>
+              <ContactDetails />
+            </RequireAuth>
+          }
+        />
+      </Routes>
     </authContext.Provider>
-    
-   
   );
 }
 
